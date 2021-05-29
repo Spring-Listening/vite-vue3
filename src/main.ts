f2d2cb800d8fd1c7b64bdf82e6790beeb8b813df
zoucw <zoucw@yunjiglobal.com>
2021-05-29 10:32:09 +0800

    修改配置

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@
  * @Author: chunwen (chunwen.zou@caibeitv.com)
  * @Date: 2021-05-28 14:44:04
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-05-28 19:16:47
+ * @LastEditTime: 2021-05-29 10:30:31
  */
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 
-import router from './router/index'
-import store from './store/index'
+import router from '@/router/index'
+import store from '@/store/index'
 
 createApp(App).use(router).use(store).mount('#app')
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,17 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: chunwen (chunwen.zou@caibeitv.com)
+ * @Date: 2021-05-28 14:44:04
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-05-29 10:31:35
+ */
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
 
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '@/router/index'
+declare module '@/store/index'
